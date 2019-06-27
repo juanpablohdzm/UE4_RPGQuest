53d4ec4e0daf8ff2b5f7dc41529b8e9270e91d93
JuanPabloH <juan@3diq.io>
2019-06-27 06:17:14 -0500

    Start AI behaviour

---
+++ Source/RPGQuest/Public/Characters/RPGEnemyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/RPGCharacter.h"
+#include "RPGEnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGQUEST_API ARPGEnemyCharacter : public ARPGCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/RPGQuest/Public/RPGAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "RPGAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGQUEST_API ARPGAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
